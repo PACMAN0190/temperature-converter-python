45b1fb000a2cad4a3d52b346b655fca66a09f40f
KHALID EL MENNANI <eelkhalid284@gmail.com>
2026-02-06 23:37:54 +0800

    Create temperature-converter-python.py

---
+++ temperature-converter-python.py
@@ -0,0 +1,13 @@
+unit = input("is this temperature celsius or fahrenheit (c/f): ")
+temp = float(input("enter the temperature: "))
+
+if unit == "c":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"the temperature in ahrenheit is: {temp}c")
+
+elif  unit == "f":
+    temp = round((9 / temp) * 5 - 32, 1)
+    print(f"the temperature in celsius is : {temp}c")
+
+else :
+    print(f"{unit}is not a valid unit")
